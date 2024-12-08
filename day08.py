88fde999f85fbd35746ac9376f97f01818b2dc38
denise <denise.eliza@hey.com>
2024-12-08 14:31:13 -0500

    day 8 part 2

--- day08.py
+++ day08.py
@@ -3,29 +3,43 @@ from collections import defaultdict
 with open('input/08.txt') as f:
     lines = [line.rstrip() for line in f]
 
-antenna_map = dict()
+y_bound = len(lines)
+x_bound = 0
 antenna_types = defaultdict(list)
 for y, this_line in enumerate(lines):
+    if not x_bound:
+        x_bound = len(this_line)
     for x, this_char in enumerate(this_line):
-        antenna_map[(x, y)] = this_char
         if this_char != '.':
+            # Keeping track of the (x, y) location of each type of antenna
             antenna_types[this_char].append((x, y))
 
-antinodes = set()
+antinodes = set()  # Using a set so duplicates are automatically removed
 for this_type in antenna_types:
     type_len = len(antenna_types[this_type])
 
+    # Compare each antenna of a type to each antenna after it in the list
     for a1 in range(0, type_len - 1):
         first = antenna_types[this_type][a1]
         for a2 in range(a1 + 1, type_len):
             second = antenna_types[this_type][a2]
-            dx = first[0] - second[0]
-            dy = first[1] - second[1]
-            antinode1 = (first[0] + dx, first[1] + dy)
-            antinode2 = (second[0] - dx, second[1] - dy)
-            if antinode1 in antenna_map:
-                antinodes.add(antinode1)
-            if antinode2 in antenna_map:
-                antinodes.add(antinode2)
+            # Add both antennas to the antinode set.
+            antinodes.add(first)
+            antinodes.add(second)
+            fx, fy = first
+            sx, sy = second
+            # Calculate the x and y distance between the first and second antenna
+            dx = fx - sx
+            dy = fy - sy
+            # Create antinodes in the positive direction from the first antenna
+            while fx + dx in range(0, x_bound) and fy + dy in range(0, y_bound):
+                antinodes.add((fx + dx, fy + dy))
+                fx += dx
+                fy += dy
+            # Create antinodes in the negative direction from the second antenna
+            while sx - dx in range(0, x_bound) and sy - dy in range(0, y_bound):
+                antinodes.add((sx - dx, sy - dy))
+                sx -= dx
+                sy -= dy
 
-print(len(antinodes))
+print(f"Part 2: {len(antinodes)}")
